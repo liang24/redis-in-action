292fdeff0642ec66e0db2bfd80bcc6c327379d8d
liang24 <380431715@qq.com>
2020-10-12 16:21:00 +0800

    Chapter01-03

---
+++ golang/Chapter03/common/common.go
@@ -0,0 +1,8 @@
+package common
+
+const (
+	OneWeekInSeconds       = 7 * 86400
+	VoteScore              = 432
+	ArticlesPerPage  int64 = 25
+	THIRTYDAYS             = 30 * 86400
+)
---
+++ golang/go.mod
@@ -0,0 +1,8 @@
+module redisInAction
+
+go 1.14
+
+require (
+	github.com/go-redis/redis/v7 v7.2.0
+	github.com/satori/go.uuid v1.2.0
+)
